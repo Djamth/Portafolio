cc5184a8fb4fafaf7a733578801a2e7cbc4db0fd
Denis Jamil <denistineohuancas@gmail.com>
2026-06-16 23:39:29 -0500

    feat(seo): mejorar configuración SEO del sitio

--- frontendv1.0.2/app/lib/site.ts
+++ frontendv1.0.2/app/lib/site.ts
@@ -22,4 +22,30 @@ export const siteConfig = {
   email: "denistineohuancas@gmail.com",
   github: "https://github.com/djamth",
   linkedin: "https://www.linkedin.com/in/denistineo",
+
+  seo: {
+    titleTemplate: "%s | Denis Jamil",              
+    defaultTitle: "Denis Jamil | Backend Developer",
+    defaultDescription: "Portafolio de Denis Jamil...",
+    keywords: ["backend", "developer", "sistemas", "portafolio","portafolio","Denis Jamil","desarrollador backend","proyectos","habilidades","experiencia"], 
+    author: "Denis Jamil Tineo Huancas",
+    robots: {
+      index: true,
+      follow: true,
+    },
+  },
+
+  og: {
+    title: "Denis Jamil | Backend Developer",
+    description: "Portafolio de Denis Jamil, desarrollador backend y estudiante de Ingeniería de Sistemas.",
+    image: "/portafolio.png",  
+    type: "website",
+    locale: "es_PE",
+    siteName: "Denis Jamil",
+    imageWidth: 1200,
+    imageHeight: 630,
+    imageAlt: "Portafolio de Denis Jamil - Backend Developer",
+  },
+
+  
 };
